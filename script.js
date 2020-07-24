1adee3b066ef1ec0e0314923006e3be2cf0ca17b
scottydont <68208416+scottydont66@users.noreply.github.com>
2020-07-24 15:10:03 -0500

    completed code for password generator

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
-
-var allChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()?{}|1234567890"
+var possibleCharChoices = '';
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerChar = "abcdefghijklmnopqrstuvwxyz"
 var numberChar = "1234567890"
 var specialChar = "!@#$%^&*()?{}|"
-let upperSplit = upperChar.split("")
-let lowerSplit = lowerChar.split("")
-let specialSplit = specialChar.split("")
-let numberSplit = numberChar.split("")
-let allSplit = allChar.split("")
-let promptCount = prompt("How many characters should your password be?")
+
+let promptCount = parseInt(prompt("How many characters should your password be?"))
+let includeLower = confirm("Do you want your password to include lowercase letters?")
+let includeUpper = confirm("Do you want your password to include uppercase letters?")
+let includeSpecial = confirm("Do you want your password to include special characters?")
+let includeNumbers = confirm("Do you want your password to include numbers?")
 
 // Write password to the #password input
 function writePassword() {
-generator(allChar)
+generator(possibleCharChoices)
 
 }
 
+// Allow character selection type
+if (includeLower === true) {
+  // generator(lowerChar + upperChar)
+  possibleCharChoices += lowerChar
+  console.log(possibleCharChoices)
+}
+
+if (includeUpper === true) {
+  // generator(lowerChar + upperChar)
+  possibleCharChoices += upperChar
+  console.log(possibleCharChoices)
+}
+
+if (includeSpecial === true) {
+  // generator(lowerChar + specialChar)
+  possibleCharChoices += specialChar
+  console.log(possibleCharChoices)
+}
+
+if (includeNumbers === true) {
+  // generator(lowerChar + numberChar)
+  possibleCharChoices += numberChar
+  console.log(possibleCharChoices)
+}
+
+
 function generator(characterArray) {
-  // let userInput = 25
   let password = ""
   for (let i = 0; i < promptCount; i++) {
     let randomIndex = Math.floor(Math.random()*characterArray.length)
@@ -30,9 +55,7 @@ function generator(characterArray) {
 
   }
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
 
